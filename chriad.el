2208cd128c525e4119dd0b603e44379512fbb991
chriad <chriad.mueller@protonmail.com>
2025-02-20 12:35:39 +0100

    Highlight expressions with inverse video

--- chriad.el
+++ chriad.el
@@ -1,3 +1,18 @@
 (use-package emacs
   :config
   (load-theme 'wheatgrass))
+;; Already declated in the init file. This second invocation is
+;; merged with the first.
+(use-package paren
+  :custom
+  (show-paren-style 'expression)
+  ;; This ingerits from show-paren-match face
+  (show-paren-match-expression '((t (:inverse-video t))))
+  :config
+  ;; priorize region overlay
+  (defadvice show-paren-function (around inhibit-in-region activate)
+    (if (region-active-p)
+	(progn
+          (delete-overlay show-paren--overlay)
+          (delete-overlay show-paren--overlay-1))
+      ad-do-it)))
